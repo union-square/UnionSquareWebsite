e5f752089a26f528736fd444177552f28c3296d8
Dominic Tayler <dominic.tayler@gmail.com>
2017-11-09 13:43:43 +0100

    Conv to PHP + img
    convert for inclused add img folder +logo

--- index.php
+++ index.php
@@ -62,17 +62,7 @@
   			</div>
   		</div>
   	</main>
-  	<footer>
-  		<div class="logoBot"></div>
-  		<nav id="menuBot">
-  			<ul>
-  				<li><a href="#">Cookies</a></li>
-  				<li><a href="#">Mention légales</a></li>
-  				<li><a href="#">Contact</a></li>
-  			</ul>
-  		</nav>
-  		<div id="sign"></div>
-  	</footer>
+  	<?php require 'footer.inc.php';?>
   	<script type="text/javascript" src="js/script.js"></script>
   </body>
 </html>
