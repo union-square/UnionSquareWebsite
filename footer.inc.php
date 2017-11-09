eca3b3b5c9958f5e5bede2c474faae6e33eaf966
Dominic Tayler <dominic.tayler@gmail.com>
2017-11-09 14:54:25 +0100

    Add Logo base + Changes to footer

---
+++ footer.inc.php
@@ -0,0 +1,15 @@
+
+    <footer>
+    	<div class="logoBot"></div>
+      <nav id="menuBot">
+    		<ul>
+        	<li><a href="#">Cookies</a></li>
+      		<li><a href="#">Mention légales</a></li>
+      		<li><a href="#">Contact</a></li>
+    		</ul>
+      </nav>
+      <div id="sign"></div>
+    </footer>
+    <script type="text/javascript" src="js/script.js"></script>
+  </body>
+</html>
