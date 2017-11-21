a70b99172d5309403626c34b37d6df218c2e6d42
@bouloan <boulord.anthony@hotmail.com>
2017-11-21 21:41:05 +0100

    séparation footer, index, header et Mise à jour Formulaire

--- incl/form.php
+++ incl/form.php
@@ -20,48 +20,36 @@ $email = new Email();
 	<title></title>
 	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 	<script>
-        function onSubmit(data) {
+        function onSubmit(data) 
+        {
             document.getElementById("contact").submit();
         }
     </script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
 	<div id="formFrame">
 		<?php
-			if(!empty($_POST)){
-				
-				echo 'hello1';
-				$varPostRecaptcha = $_POST['g-recaptcha-response'];
-			
-				$response = $recaptcha->verifyResponse($_POST['g-recaptcha-response']);
-				// si le formulaire a été posté
-				echo 'hello2';
-			
-			//vérification reCaptcha 
-			
-			if(!empty($varPostRecaptcha)){
-			echo 'hello3';
+		if(!empty($_POST)){
+			$postRecaptcha = $_POST['g-recaptcha-response'];
+			$response = $recaptcha->verifyResponse($_POST['g-recaptcha-response']);
+
+			if(!empty($postRecaptcha)){
 			//on vérifie si les données renseignées sont valides
 			$checkSubmittedData->checkForm($formData);
-				//si les données renseignées sont valides
+				
 				if ($checkSubmittedData->getFormDataValidation() == true){
 					//si une erreur est survenu au niveau du recaptcha
 					if(isset($response['success']) and $response['success'] != true) {
-						echo "An Error Occured and Error code is :".$response['error-codes'];
+						echo "Une erreur est survenue. Le code de l'erreur est :".$response['error-codes'];
 					}
 					//si le recaptcha a validé l'envoi
-					else {
-					
+					else {	
 					//on envoie un mail au client indiquant que la demande a bien été prise en compte
-					//on inscrit l'adresse du destinaitaire
 					$email->setAddresseeEmail("boulord.anthony@hotmail.com");
-					// on inscrit  l'adresse de l'expéditeur (c'està dire nous)
 					$email->setHeader("Anthony", "boulord.anthony@gmail.com", "Anthony", "boulord.anthony@gmail.com");
-					//on inscrit le sujet du mail
 					$email->setSubject("nous avons bien reçu votre demande");
-
 					$email->setMessage("<p>Bonjour ".$_POST['contactName'].",</br></br>
 						Vous êtes entré en contact avec le collectif <b>Union Square</b> et nous vous en remercions. </br></br>
 						Cette email vous a été envoyé automatiquement pour vous confirmer que votre demande nous a bien été transmise.</br></br>
@@ -69,16 +57,14 @@ $email = new Email();
 						A très bientôt,</br></br>
 						Union Square</br></br>
 						</p>");
-					//on expédie le mail
 					$email->sentEmail();
 					
-					//on s'envoie une mail contenant le message du client		
+					//on s'envoie un mail contenant le message du client		
 					$email->setAddresseeEmail("boulord.anthony@hotmail.com");
 					$email->setHeader("Anthony", "boulord.anthony@gmail.com", "Anthony", "boulord.anthony@gmail.com");
 					$email->setSubject("message du client: ".$_POST['contactName']."");
 					$email->setMessage("".$_POST['message']."");
 					$email->sentEmail();
-					
 					}
 					?>
 					<form id="contact" method="post">
@@ -92,17 +78,16 @@ $email = new Email();
 						echo'<div id="formSucceedMessage">
 						<p>Votre message nous a été transmis, vous recevrez un email pour confirmer l\'envoi de votre message</p>
 						<p>Nous vous recontacterons au plus vite. A bientôt!</p>
-					</div>';
+						</div>';
 					} else{
 						echo '<div id="formFailedMessage">
 						<p>Une erreur est survenue, votre message n\'a pu nous être délivré</p>
 						<p>Veuillez essayer à nouveau ou nous contacter par téléphone au 06.89.21.07.19</p>
-					</div>';
+						</div>';
 					}
 				} 
 				// si les données renseignée ne sont pas valides
 				else{
-					//on affiche les messages d'erreur
 					echo $checkSubmittedData->getErrorMessage();
 					//on enregistrer les valeurs qui sont valides afin de ne pas avoir à les renseignées à nouveau
 					$formData->setFormDataValue($_POST);
@@ -112,20 +97,18 @@ $email = new Email();
 						$form->setFormElement($formData);
 						?>
 					</form>		
-					<?php } 
-				
+					<?php 
+				} 
 			}
-		}
-			//si le formulaire n'a pas été envoyé ou comporte des erreurs, on propose le formulaire à l'utilisateur
-			else{
+		} else{
+			?>
+			<form id="contact" method="post">
+				<?php
+				$form->setFormElement($formData);
 				?>
-				<form id="contact" method="post">
-					<?php
-					$form->setFormElement($formData);
-					?>
-				</form>		
-			<?php } ?>
-		
+			</form>		
+			<?php 
+		} ?>
 	</div>
 
 	<script type="text/javascript" src="js/form.js"></script>
--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-	<title>Union Square</title>
-	<meta charset="UTF-8">
-    <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  </head>
-  <body>
-  	<header>
-  		<div id="mainLogo"></div>
-  		<nav id="mainMenu">
-  			<ul>
-  				<li><a href="#">Acceuil</a></li>
-  				<li><a href="#">Equipe</a></li>
-  				<li><a href="#">Contact & devis</a></li>
-  			</ul>
-  		</nav>
-  	</header>
-  	<main>
+<?php require'incl/header.php' ?>
   		<div id="intro">
         <div id="introBlock">
           <h1>Solution complète</h1>
@@ -28,6 +8,9 @@
           </p>
           </div>
   		</div>
+  		<svg id="svgBl" height="90" width="300">
+        <polygon points="0,90 300,0 0,0" style="fill:rgba(216,216,216,0.15);stroke:rgba(216,216,216,0.15);stroke-width:1" />
+      </svg>
   		<div id="content">
   			<div class="exp">
           <div class="expTxtBox">
@@ -37,20 +20,22 @@
               Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
             </p>
           </div>
-  				
-  				<div class="ilu" id="iluTop"></div>		
-  			</div>
-  			<div class="exp" id="expMid">
-  				<div class="ilu" id="iluMid"></div>
-          <div class="expTxtBox">
-            <h2>Web design (ui / ux)</h2>
-            <span class="sep"></span>
-            <p class="expTxt">
-              Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
-            </p>
-        </div>
+  			<div class="ilu" id="iluTop"></div>		
   		</div>
-  			<div class="exp">
+  		<svg height="90" width="300">
+          <polygon points="0,90 300,0 0,0" style="fill:white" />
+        </svg>
+  		<div class="exp" id="expMid">
+  			<div class="ilu" id="iluMid"></div>
+          	<div class="expTxtBox">
+            	<h2>Web design (ui / ux)</h2>
+            	<span class="sep"></span>
+	            <p class="expTxt">
+	              Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+	            </p>
+        	</div>
+  		</div>
+  		<div class="exp">
           <div class="expTxtBox">
             <h2>SEO & webmarketing</h2>
             <span class="sep"></span>
@@ -61,18 +46,4 @@
   				<div class="ilu" id="iluBot"></div>
   			</div>
   		</div>
-  	</main>
-  	<footer>
-  		<div class="logoBot"></div>
-  		<nav id="menuBot">
-  			<ul>
-  				<li><a href="#">Cookies</a></li>
-  				<li><a href="#">Mention légales</a></li>
-  				<li><a href="#">Contact</a></li>
-  			</ul>
-  		</nav>
-  		<div id="sign"></div>
-  	</footer>
-  	<script type="text/javascript" src="js/script.js"></script>
-  </body>
-</html>
+<?php require 'incl/footer.php';?>
